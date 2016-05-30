ac24ca473cbe01225ed65b0d58749f71e7aac3ab
Reshama <reshama@thisismetis.com>
2016-05-30 15:39:29 -0400

    Update q8_parsing.py
    removing hints since it's causing confusion for users.  Many ask questions about why import csv or "self" in function.

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,6 @@
-#The football.csv file contains the results from the English Premier League. 
+# The football.csv file contains the results from the English Premier League. 
 # The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
-# The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
-
-
-import csv
-
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
-
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
-
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
