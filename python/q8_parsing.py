8e68c0464338cdc6dd3ad4e60fe9fd8d1a6ca1e2
Ibrahim Gabr <igabr@uchicago.edu>
2017-12-11 13:21:41 +0000

    Delete q8_parsing.py
    removed

--- python/q8_parsing.py
+++
@@ -1,6 +0,0 @@
-# The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
-
